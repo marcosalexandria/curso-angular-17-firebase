18a09dbb52b7edae381a4fb69f4f9ae32efff9df
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-06 20:31:34 -0300

    fix: aula 11 Como funcionam os componentes do Angular
    estilizando e editando componente buttom

--- crud-usuarios/src/app/components/button/button.component.ts
+++ crud-usuarios/src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
-
+  //@Input() é para dizer que essa varial será recebida pelo componente pai
+  @Input() textoBotao: string;
 }
