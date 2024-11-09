b50e6b38cb0d7a09caa3927d201080f48cf9058a
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-09 18:00:56 -0300

    configurando environment

--- crud-usuarios/src/environments/environment.ts
+++ crud-usuarios/src/environments/environment.ts
@@ -1,1 +1,10 @@
-export const environment = {};
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyAqPSLTBLF52dqqLV8lo15Q5PFfaKfP884",
+    authDomain: "crud-usuarios-angular-95183.firebaseapp.com",
+    projectId: "crud-usuarios-angular-95183",
+    storageBucket: "crud-usuarios-angular-95183.firebasestorage.app",
+    messagingSenderId: "695940996787",
+    appId: "1:695940996787:web:c3125ca956d2c493c59ac6"
+  }
+};
