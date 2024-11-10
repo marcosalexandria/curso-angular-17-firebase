028e64fb7e8298fde30caabb751ed4731265b651
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-10 11:36:32 -0300

    fix: tipa as variáveis de user. estancia um user na mão
    aula: 26. Criando uma Interface de usuários - Extra *

--- crud-usuarios/src/app/services/users.service.ts
+++ crud-usuarios/src/app/services/users.service.ts
@@ -1,3 +1,4 @@
+import { User } from './../interfaces/user';
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore' //banco de dados do firebase
 import { Observable } from 'rxjs';
@@ -9,17 +10,24 @@ export class UsersService {
 
   constructor(private dataBaseStore: AngularFirestore) { }
 
+  user: User = {
+    name: 'Marcos',
+    email: 'marcos@email.com',
+    sector: 'Tecnologia',
+    role: 'Desenvolvedor'
+  }
+
   getAllUsers(){
       //          obtem todos da coleção('users', ordena o que vier por('nome')).pega o({idField: muda o nome para 'firebaseId'})
                                                                                                                     //é um array de Observable
     return this.dataBaseStore.collection('users', user => user.orderBy('name')).valueChanges({idField: 'firebaseId'}) as Observable<any[]>
   }
 
-  addUsers(user: any){
+  addUsers(user: User){
     return this.dataBaseStore.collection('users').add(user);
   }
 
-  update(userId: string, user: any){
+  update(userId: string, user: User){
     return this.dataBaseStore.collection('users').doc(userId).update(user);
   }
 
