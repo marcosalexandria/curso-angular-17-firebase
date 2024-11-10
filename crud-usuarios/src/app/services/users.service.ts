f97a37b92a082635fb5337cbba90168735287233
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-10 11:08:20 -0300

    feat: cria os métodos de crud do service
    aula: 25. Criando o Serviço (service) do nosso CRUD no Firebase

--- crud-usuarios/src/app/services/users.service.ts
+++ crud-usuarios/src/app/services/users.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore' //banco de dados do firebase
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private dataBaseStore: AngularFirestore) { }
+
+  getAllUsers(){
+      //          obtem todos da coleção('users', ordena o que vier por('nome')).pega o({idField: muda o nome para 'firebaseId'})
+                                                                                                                    //é um array de Observable
+    return this.dataBaseStore.collection('users', user => user.orderBy('name')).valueChanges({idField: 'firebaseId'}) as Observable<any[]>
+  }
+
+  addUsers(user: any){
+    return this.dataBaseStore.collection('users').add(user);
+  }
+
+  update(userId: string, user: any){
+    return this.dataBaseStore.collection('users').doc(userId).update(user);
+  }
+
+  deleteUser(userId: string){
+    return this.dataBaseStore.collection('users').doc(userId).delete();
+  }
 }
