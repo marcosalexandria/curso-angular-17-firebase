1a6fe3a8a358330d42b60a77fccf2c8b5da1ad69
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-15 10:18:41 -0300

    feat: cria textos botão e filtro de crud
    aula: 28. Pt.1 - Montando estrutura da tela CRUD (HTML + CSS)

--- crud-usuarios/src/app/pages/crud/crud.component.ts
+++ crud-usuarios/src/app/pages/crud/crud.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-crud',
   templateUrl: './crud.component.html',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class CrudComponent {
 
+  dataSource: any;
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
